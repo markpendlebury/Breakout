12c71c24daaf7428db2ec616b1c034e43af7d79c
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2023-08-21 19:45:34 +0100

    Implementing basic main menu

---
+++ Assets/Code/Menus/Main/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OnNewGameButton()
+    {
+        Debug.Log("Starting a New Game...");
+        SceneManager.LoadScene(1);
+    }
+
+
+    public void OnQuitButton()
+    {
+        Debug.Log("Quitting...");
+        Application.Quit();
+    }
+}
